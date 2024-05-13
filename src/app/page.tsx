d986d166e9cbc1e83d503c7b97b743c10d0147d1
Alberto Polanco <japolancoc@hotmail.com>
2024-05-12 23:23:14 -0600

    Probando NextJs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
+      {/* <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
@@ -26,7 +26,7 @@ export default function Home() {
             />
           </a>
         </div>
-      </div>
+      </div> */}
 
       <div className={styles.center}>
         <Image
@@ -39,7 +39,7 @@ export default function Home() {
         />
       </div>
 
-      <div className={styles.grid}>
+      {/* <div className={styles.grid}>
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className={styles.card}
@@ -89,7 +89,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-      </div>
+      </div> */}
     </main>
   );
 }
